6dc3d26291ff6f2ce27eb7b543ae5c8b21faaac4
Antonio Gonzalez <antgonza@gmail.com>
2022-08-24 10:18:36 -0600

    mv to slurm

--- sequence_processing_pipeline/QCJob.py
+++ sequence_processing_pipeline/QCJob.py
@@ -3,6 +3,7 @@ from os import walk, stat, listdir, makedirs
 from os.path import exists, join, split
 from sequence_processing_pipeline.Job import Job
 from sequence_processing_pipeline.PipelineError import PipelineError
+from sequence_processing_pipeline.QCHelper import QCHelper
 from shutil import move
 import logging
 import re
@@ -12,10 +13,9 @@ logging.basicConfig(level=logging.DEBUG)
 
 class QCJob(Job):
     def __init__(self, fastq_root_dir, output_path, sample_sheet_path,
-                 minimap_database_paths, kraken2_database_path, queue_name,
-                 node_count, nprocs, wall_time_limit, jmem, fastp_path,
-                 minimap2_path, samtools_path, modules_to_load, qiita_job_id,
-                 pool_size, max_array_length):
+                 mmi_db_path, queue_name, node_count, nprocs, wall_time_limit,
+                 jmem, fastp_path, minimap2_path, samtools_path,
+                 modules_to_load, qiita_job_id, pool_size, max_array_length):
         """
         Submit a Torque job where the contents of fastq_root_dir are processed
         using fastp, minimap, and samtools. Human-genome sequences will be
@@ -23,8 +23,7 @@ class QCJob(Job):
         :param fastq_root_dir: Path to a dir of Fastq files, org. by project.
         :param output_path: Path where all pipeline-generated files live.
         :param sample_sheet_path: Path to a sample sheet file.
-        :param minimap_database_paths: Path to human genome databases in env.
-        :param kraken2_database_path: Path to human genome dat abase in env.
+        :param mmi_db_path: Path to human genome database in running env.
         :param queue_name: Torque queue name to use in running env.
         :param node_count: Number of nodes to use in running env.
         :param nprocs: Number of processes to use in runing env.
@@ -49,12 +48,12 @@ class QCJob(Job):
         self.sample_ids = metadata['sample_ids']
         self.project_data = metadata['projects']
         self.needs_trimming = metadata['needs_adapter_trimming']
-        self.nprocs = 16 if nprocs > 16 else nprocs
+        self.nprocs = nprocs
         self.chemistry = metadata['chemistry']
-        self.minimap_database_paths = minimap_database_paths
-        self.kraken2_database_path = kraken2_database_path
+        self.mmi_db_path = mmi_db_path
         self.queue_name = queue_name
         self.node_count = node_count
+        self.nprocs = nprocs
         self.wall_time_limit = wall_time_limit
         self.jmem = jmem
         self.fastp_path = fastp_path
@@ -68,34 +67,12 @@ class QCJob(Job):
 
         self.script_paths = {}
 
-        if not isinstance(self.needs_trimming, bool):
-            raise ValueError("needs_adapter_trimming must be boolean.")
-
         for project in self.project_data:
             project_name = project['Sample_Project']
             fastq_files = self._find_fastq_files(project_name)
             if not fastq_files:
                 raise PipelineError("Fastq files could not be found for "
                                     f"project '{project_name}'")
-
-            if project['ForwardAdapter'] == 'NA':
-                project['ForwardAdapter'] = None
-
-            if project['ReverseAdapter'] == 'NA':
-                project['ReverseAdapter'] = None
-
-            if project['ForwardAdapter'] is None:
-                if project['ReverseAdapter'] is not None:
-                    raise ValueError(("ForwardAdapter is declared but not "
-                                      "ReverseAdapter."))
-
-            if project['ReverseAdapter'] is None:
-                if project['ForwardAdapter'] is not None:
-                    raise ValueError(("ReverseAdapter is declared but not "
-                                      "ForwardAdapter."))
-
-            if not isinstance(project['HumanFiltering'], bool):
-                raise ValueError("needs_adapter_trimming must be boolean.")
 
             script_path = self._generate_job_script(project_name,
                                                     project['ForwardAdapter'],
@@ -133,9 +110,10 @@ class QCJob(Job):
         for project in self.project_data:
             project_name = project['Sample_Project']
             needs_human_filtering = project['HumanFiltering']
-            pbs_job_id = self.qsub(self.script_paths[project_name], None, None,
-                                   exec_from=self.log_path, callback=callback)
-            logging.debug(f'QCJob {pbs_job_id} completed')
+            job_id = self.submit_job(
+                self.script_paths[project_name], None, None,
+                exec_from=self.log_path, callback=callback)
+            logging.debug(f'QCJob {job_id} completed')
             source_dir = join(self.output_path, project_name)
             if needs_human_filtering is True:
                 filtered_directory = join(source_dir, 'filtered_sequences')
@@ -248,37 +226,42 @@ class QCJob(Job):
                              h_filter, fastq_file_paths):
         lines = []
 
+        project_dir = join(self.output_path, project_name)
+
         details_file_name = f'{self.job_name}_{project_name}.array-details'
         sh_details_fp = join(self.output_path, details_file_name)
 
-        cmds = self._generate_commands(fastq_file_paths, project_name,
-                                       join(self.output_path, project_name),
-                                       h_filter, adapter_a, adapter_A)
+        qc = QCHelper(self.nprocs, fastq_file_paths, project_name,
+                      project_dir, self.mmi_db_path, adapter_a,
+                      adapter_A, self.needs_trimming, h_filter,
+                      self.chemistry, self.fastp_path, self.minimap2_path,
+                      self.samtools_path)
+
+        cmds = qc.generate_commands()
         cmds = self._group_commands(cmds)
 
         lines.append("#!/bin/bash")
         job_name = f'{self.qiita_job_id}_{self.job_name}_{project_name}'
 
-        lines.append(f"#PBS -N {job_name}")
-        lines.append("#PBS -q %s" % self.queue_name)
-        lines.append("#PBS -l nodes=%d:ppn=%d" % (self.node_count,
-                                                  self.nprocs))
-        lines.append("#PBS -V")
-        lines.append("#PBS -l walltime=%d:00:00" % self.wall_time_limit)
-        lines.append(f"#PBS -l mem={self.jmem}")
+        lines.append(f"#SBATCH --job-name {job_name}")
+        lines.append(f"#SBATCH -p {self.queue_name}")
+        lines.append(f'#SBATCH -N {self.node_count}')
+        lines.append(f'#SBATCH -n {self.nprocs}')
+        lines.append("#SBATCH --time {self.wall_time_limit}:00:00")
+        lines.append(f"SBATCH --mem={self.jmem}")
 
-        lines.append("#PBS -t 1-%d%%%d" % (len(cmds), self.pool_size))
+        lines.append("#SBATCH --array 1-%d%%%d" % (len(cmds), self.pool_size))
 
         lines.append("set -x")
         lines.append('date')
         lines.append('hostname')
-        lines.append('echo ${PBS_JOBID} ${PBS_ARRAYID}')
+        lines.append('echo ${SLURM_JOBID} ${SLURM_ARRAY_TASK_ID}')
         lines.append(f'cd {self.output_path}')
 
         if self.modules_to_load:
             lines.append("module load " + ' '.join(self.modules_to_load))
 
-        lines.append('offset=${PBS_ARRAYID}')
+        lines.append('offset=${SLURM_ARRAY_TASK_ID}')
         lines.append('step=$(( $offset - 0 ))')
         lines.append(f'cmd0=$(head -n $step {sh_details_fp} | tail -n 1)')
         lines.append('eval $cmd0')
@@ -293,153 +276,3 @@ class QCJob(Job):
             f.write('\n'.join(cmds))
 
         return job_script_path
-
-    def _generate_commands(self, fastq_file_paths, project_name, project_dir,
-                           h_filter, adapter_a, adapter_A):
-        """
-        Generate the command-lines needed to QC the data, based on the
-        parameters supplied to the object. The result will be the list of
-        strings needed to process the fastq files referenced in the trim file.
-        :return: A list of strings that can be run using Popen() and the like.
-        """
-        fastp_reports_dir = join(project_dir, 'fastp_reports_dir')
-
-        # if this directory is not made, then fastp will not create the html
-        # and json directories and generate output files for them.
-        makedirs(fastp_reports_dir, exist_ok=True)
-
-        cmds = []
-
-        od = 'filtered_sequences' if h_filter is True else 'trimmed_sequences'
-
-        if self.needs_trimming is True:
-            if not exists(join(project_dir, od)):
-                makedirs(join(project_dir, od),
-                         exist_ok=True)
-
-        if self.needs_trimming is True:
-            foo = [x.strip() for x in fastq_file_paths if '_R1_' in x]
-            for fastq_file_path in foo:
-                current_dir = split(fastq_file_path)[0]
-                _, filename1 = split(fastq_file_path)
-                filename2 = filename1.replace('_R1_00', '_R2_00')
-
-                if h_filter is True:
-                    cmds.append(self._gen_chained_cmd(current_dir,
-                                                      filename1,
-                                                      filename2,
-                                                      fastp_reports_dir,
-                                                      project_name,
-                                                      project_dir,
-                                                      adapter_a,
-                                                      adapter_A))
-                else:
-                    cmds.append(self._gen_fastp_cmd(current_dir,
-                                                    filename1,
-                                                    filename2,
-                                                    project_dir,
-                                                    adapter_a,
-                                                    adapter_A))
-            return cmds
-
-        logging.warning("QCJob created w/a_trim set to False.")
-        return None
-
-    def _gen_fastp_cmd(self, current_dir, filename1, filename2, project_dir,
-                       adapter_a, adapter_A):
-        """
-        Generates a command-line string for running fastp, based on the
-         parameters supplied to the object.
-        :return: A string suitable for executing in Popen() and the like.
-        """
-        read1_input_path = join(current_dir, filename1)
-        read2_input_path = join(current_dir, filename2)
-
-        partial_path = join(current_dir, project_dir)
-        json_output_path = join(partial_path, 'json',
-                                filename1.replace('.fastq.gz', '.json'))
-        html_output_path = join(partial_path, 'html',
-                                filename1.replace('.fastq.gz', '.html'))
-        read1_output_path = join(partial_path, 'trimmed_sequences',
-                                 filename1.replace('.fastq.gz',
-                                                   '.fastp.fastq.gz'))
-        read2_output_path = join(partial_path, 'trimmed_sequences',
-                                 filename2.replace('.fastq.gz',
-                                                   '.fastp.fastq.gz'))
-        report_title = filename1.replace('.fastq.gz', '') + '_report'
-
-        result = self.fastp_path
-        if adapter_a:
-            # assume that if adapter_a is not None, then adapter_A is not None
-            # as well. We performed this check already.
-            result += (f' --adapter_sequence {adapter_a}'
-                       f' --adapter_sequence_r2 {adapter_A}')
-
-        result += (f' -l 100 -i {read1_input_path} -I {read2_input_path} -w '
-                   f'{self.nprocs} -j {json_output_path} -h {html_output_path}'
-                   f' -o {read1_output_path} -O {read2_output_path} -R '
-                   f'{report_title}')
-
-        return result
-
-    def _gen_chained_cmd(self, current_dir, filename1, filename2,
-                         fastp_reports_dir, project_name, project_dir,
-                         adapter_a, adapter_A):
-        read1_input_path = join(current_dir, filename1)
-        read2_input_path = join(current_dir, filename2)
-
-        tmp_path = join(project_name, fastp_reports_dir, 'json')
-        makedirs(tmp_path, exist_ok=True)
-        json_output_path = join(tmp_path, filename1.replace('.fastq.gz',
-                                                            '.json'))
-
-        tmp_path = join(project_name, fastp_reports_dir, 'html')
-        makedirs(tmp_path, exist_ok=True)
-        html_output_path = join(tmp_path, filename1.replace('.fastq.gz',
-                                                            '.html'))
-
-        partial = join(project_dir, 'filtered_sequences')
-
-        path1 = join(partial, filename1.replace('.fastq.gz',
-                                                '.trimmed.fastq.gz'))
-        path2 = join(partial, filename2.replace('.fastq.gz',
-                                                '.trimmed.fastq.gz'))
-
-        kraken_report_path = join(partial,
-                                  filename1.replace('.fastq.gz',
-                                                    '.kraken2_report.txt'))
-
-        kraken_output_path = join(partial,
-                                  filename1.replace('.fastq.gz',
-                                                    '.kraken2.trimmed.#.fastq')
-                                  )
-
-        result = self.fastp_path
-
-        if adapter_a:
-            # assume that if adapter_a is not None, then adapter_A is not None
-            # as well. We performed this check already.
-            result += (f' --adapter_sequence {adapter_a}'
-                       f' --adapter_sequence_r2 {adapter_A}')
-
-        result += (f' -l 100 -i {read1_input_path} -I {read2_input_path} -w '
-                   f'{self.nprocs} -j {json_output_path} -h {html_output_path}'
-                   ' --stdout ')
-
-        # for each database specified in configuration, run the data through
-        # minimap using the database and pipe to samtools to tame the output.
-        for database in self.minimap_database_paths:
-            result += (f'| {self.minimap2_path} -ax sr -t {self.nprocs} '
-                       f'{database} - -a | {self.samtools_path} fastq -@ '
-                       f'{self.nprocs} -f 12 -F 256 ')
-
-        # append the final parameters to write out the final output to disk
-        result += f'-1 {path1} -2 {path2}'
-
-        # add krakken2
-        result += (f'\nkraken2 --threads {self.nprocs} --db '
-                   f'{self.kraken2_database_path} --report '
-                   f'{kraken_report_path} --unclassified-out '
-                   f'{kraken_output_path} --paired {path1} {path2}')
-
-        return result
